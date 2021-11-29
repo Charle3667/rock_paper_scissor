59ef6b2a28640889e759ed3efe6ffe63fe6b5e1b
Charlie Fordon <fordoncf@gmail.com>
2021-11-29 17:41:31 -0500

    Game can now be played. Score keeping not implemented

--- code.js
+++ code.js
@@ -1,1 +1,60 @@
-console.log("Hello World")
+console.log("Rock, Paper, Scissors")
+let playerSelection = prompt('Choose "Rock", "Paper" or "Scissors"')
+game()
+
+function game () {
+let random = Math.floor(Math.random()*3) + 1;
+let move = computerPlay()
+let result = singleRound()
+console.log("Player move is " + playerSelection)
+console.log("Computer move is " + move)
+console.log("Result is: " + '"' + result + '"')
+alert(result)
+
+
+    function computerPlay() {
+        if (random === 1) {  
+            return("Rock")}
+        else if (random ===2){
+            return("Paper")
+        }
+        else {
+            return("Scissors")
+        } 
+    }
+    function singleRound(){
+        if (playerSelection === "Rock") {
+            if (move === "Rock") {
+                return("Tie.")}
+            else if (move === "Paper"){ 
+                return("Loss. Paper beats rock.")
+            }
+            else {return("Win. Rock beats Scissors")}
+            
+        }
+        if (playerSelection === "Paper") {
+            if (move === "Rock") {
+                return("Win. Paper beats rock.")}
+            else if (move === "Paper"){ 
+                return("Tie.")
+            }
+            else {return("Loss. Scissors beats Paper")}
+        }
+        if (playerSelection === "Scissors") {
+            if (move === "Rock") {
+                return("Loss. Rock beats scissors.")}
+            else if (move === "Paper"){ 
+                return("Win. Scissors beats paper.")
+            }
+            else {return("Tie.")}        
+        }       
+    }
+}
+
+
+
+
+
+
+
+
