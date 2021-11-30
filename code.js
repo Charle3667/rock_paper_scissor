20131e8e6511fb16242d14f263648dd35cd6d13d
Charlie Fordon <fordoncf@gmail.com>
2021-11-30 17:14:24 -0500

    working rock paper scissors game made with javascript

--- code.js
+++ code.js
@@ -1,18 +1,57 @@
 console.log("Rock, Paper, Scissors")
-let playerSelection = prompt('Choose "Rock", "Paper" or "Scissors"')
+var computerScore = 0
+var playerScore = 0
+
 game()
 
-function game () {
-let random = Math.floor(Math.random()*3) + 1;
-let move = computerPlay()
-let result = singleRound()
-console.log("Player move is " + playerSelection)
-console.log("Computer move is " + move)
-console.log("Result is: " + '"' + result + '"')
-alert(result)
+function game() {
+    if (playerScore === 5) {
+        console.log("Player wins. Congrats!")
+        alert("Player wins. Congrats!")
+    }
+    else if (computerScore === 5) {
+        console.log("Computer wins. Try again.")
+        alert("Computer wins. Try again.")
+    }
+    else {fullGame()}
+}
+
+function fullGame() { 
+    let playerSelection = prompt('Choose "Rock", "Paper" or "Scissors"')
+    let result = singleRound()
+    console.log(result)
+    alert(result)
+    
+    if (result === "Win. Rock beats scissors.") {
+        playerScore = (playerScore + 1) 
+    }
+    else if (result === "Win. Paper beats rock.") {
+        playerScore = (playerScore + 1) 
+    }
+    else if (result === "Win. Scissors beats paper.") {
+        playerScore = (playerScore + 1) 
+    }
+    else if (result === "Loss. Rock beats scissors.") {
+        computerScore = (computerScore + 1)
+    }
+    else if (result === "Loss. Paper beats rock.") {
+        computerScore = (computerScore + 1)
+    }
+    else if (result === "Loss. Scissors beats paper.") {
+        computerScore = (computerScore + 1)
+    }
+    else {}
+
+    console.log("Score is: Player - " + playerScore + " and Computer - " + computerScore + ".")
+    alert("Score is: Player - " + playerScore + " and Computer - " + computerScore + ".")
+    game()
+
+
+
 
 
     function computerPlay() {
+        let random = Math.floor(Math.random()*3) + 1;
         if (random === 1) {  
             return("Rock")}
         else if (random ===2){
@@ -22,34 +61,37 @@ alert(result)
             return("Scissors")
         } 
     }
+
+
     function singleRound(){
-        if (playerSelection === "Rock") {
-            if (move === "Rock") {
+        let move = computerPlay()
+        if (playerSelection.toUpperCase() === "Rock".toUpperCase()) {
+            if (move.toUpperCase() === "Rock".toUpperCase()) {
                 return("Tie.")}
-            else if (move === "Paper"){ 
+            else if (move.toUpperCase() === "Paper".toUpperCase()){ 
                 return("Loss. Paper beats rock.")
             }
-            else {return("Win. Rock beats Scissors")}
-            
+            else {return("Win. Rock beats scissors.")} 
         }
-        if (playerSelection === "Paper") {
-            if (move === "Rock") {
+        if (playerSelection.toUpperCase() === "Paper".toUpperCase()) {
+            if (move.toUpperCase() === "Rock".toUpperCase()) {
                 return("Win. Paper beats rock.")}
-            else if (move === "Paper"){ 
+            else if (move.toUpperCase() === "Paper".toUpperCase()){ 
                 return("Tie.")
             }
-            else {return("Loss. Scissors beats Paper")}
+            else {return("Loss. Scissors beats paper.")}
         }
-        if (playerSelection === "Scissors") {
-            if (move === "Rock") {
+        if (playerSelection.toUpperCase() === "Scissors".toUpperCase()) {
+            if (move.toUpperCase() === "Rock".toUpperCase()) {
                 return("Loss. Rock beats scissors.")}
-            else if (move === "Paper"){ 
+            else if (move.toUpperCase() === "Paper".toUpperCase()){ 
                 return("Win. Scissors beats paper.")
             }
             else {return("Tie.")}        
         }       
     }
 }
+
 
 
 
